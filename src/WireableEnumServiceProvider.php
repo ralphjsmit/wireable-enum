6d1ab487157e4ed824b8369638902ae519eef736
ralphjsmit <ralphjsmit@users.noreply.github.com>
2021-12-18 16:17:35 +0000

    Fix styling

--- src/WireableEnumServiceProvider.php
+++ src/WireableEnumServiceProvider.php
@@ -3,9 +3,7 @@
 namespace RalphJSmit\WireableEnum;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class WireableEnumServiceProvider extends ServiceProvider
 {
-
 }
